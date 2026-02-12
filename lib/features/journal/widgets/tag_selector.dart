6605794776d290d27e06d187c794f53a50484ff0
yyinhsu <yyinhsu@gmail.com>
2026-02-12 23:44:48 +0800

    fix(journal): correct import paths in tag selector
    Update imports to use correct paths after project restructuring:
    - core/theme instead of features/core/theme
    - models/ instead of features/models/
    - features/journal/tag_providers instead of journal/widgets/tag_providers

--- lib/features/journal/widgets/tag_selector.dart
+++ lib/features/journal/widgets/tag_selector.dart
@@ -1,10 +1,9 @@
+import 'package:cultainer/core/theme/app_colors.dart';
+import 'package:cultainer/core/theme/app_typography.dart';
+import 'package:cultainer/features/journal/tag_providers.dart';
+import 'package:cultainer/models/tag.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:cultainer/features/core/theme/app_colors.dart';
-import 'package:cultainer/features/core/theme/app_typography.dart';
-import 'package:cultainer/features/models/tag.dart';
-import 'package:cultainer/features/journal/widgets/tag_providers.dart';
 
 /// A widget for selecting tags from a list.
 class TagSelector extends ConsumerStatefulWidget {
